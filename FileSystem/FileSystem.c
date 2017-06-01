91df4994bf943a06a080e8184eaa73930801e689
ezequieliriso <ezequieliriso@gmail.com>
2017-06-01 16:53:02 -0300

    Falta gestionar Respuestas con Kernel

--- FileSystem/FileSystem.c
+++ FileSystem/FileSystem.c
@@ -5,58 +5,40 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <dirent.h>
 #include <netinet/in.h>
 #include <netdb.h>
 #include <arpa/inet.h>
 #include "../config_shortcuts/config_shortcuts.h"
+#include "socketEze.h"
+
+fs_config data_config;
+
+uint32_t sizeFile(char *nombreArchivo){
+	FILE *archivo;
+	archivo = fopen(nombreArchivo,"r");
+	if(!archivo)
+		return -1;
+	fseek(archivo,0,SEEK_END);
+	uint32_t dimension = ftell(archivo);
+	fclose(archivo);
+	return dimension;
+}
 
 
-/*Sockets para recibir mensaje del Kernel*/
-
-	/*Funciones*/
-
-	//Backlog es la cantidad maxima que quiero de conexiones pendientes
-
-	void poner_a_escuchar(int sockfd, struct sockaddr* server, int backlog)
-	{
-		if((bind(sockfd, server, sizeof(struct sockaddr))) == -1)
-			{
-				perror("bind");
-				exit(1);
-			}
-		if(listen(sockfd,backlog) == -1)
-			{
-				perror("listen");
-				exit(1);
-			}
-		return;
-	}
-
-	struct sockaddr_in crear_estructura_server (int puerto)
-	{
-		struct sockaddr_in server;
-		server.sin_family = AF_INET;
-		server.sin_port = htons(puerto);
-		server.sin_addr.s_addr = INADDR_ANY;
-		memset(&(server.sin_zero),'\0',8);
-		return server;
-	}
-
-	int aceptar_conexion(int sockfd, struct sockaddr* clien)
-	{
-		int socknuevo;
-		socklen_t clie_len=sizeof(struct sockaddr_in);
-		if((socknuevo = accept(sockfd, clien, &clie_len)) == -1)
-		{
-			perror("accept");
-		}
-		return socknuevo;
-	}
+uint32_t exist(char *nombreArchivo){
+	FILE *archivo;
+	archivo = fopen(nombreArchivo,"r");
+	uint32_t var = true;
+	if(!archivo)
+		var = false;
+	fclose(archivo);
+	return var;
+}
 
 int main(int argc, char** argv)
 {
 	t_config *config;
-	fs_config data_config;
 
 	checkArguments(argc);
 	char *cfgPath = malloc(sizeof("../../FileSystem/") + strlen(argv[1])+1);
@@ -72,85 +54,22 @@ int main(int argc, char** argv)
 	printf("PORT = %s\n", data_config.puerto);
 	printf("Montaje = %s\n", data_config.montaje);
 
-	int portnum;
-	portnum=atoi(data_config.puerto); /*Lo asigno antes de destruir config*/
 
 	config_destroy(config);		//Eliminamos fs_config, liberamos la memoria que utiliza
 
-	int listener, newfd, bytes_leidos, bytes;
-	char buf[256];
-	struct sockaddr_in server, cliente;
-	int pid = getpid();
+	//todo el server declaradito aca
+	uint32_t miSocket = server(atoi(data_config.puerto),1);
+	//end
 
-	/*inicializo el buffer*/
+	aceptarCliente(miSocket,1);
+	DIR *mount = opendir(data_config.montaje);
+	if(!mount)
+		exit(3);
 
-	memset(buf, 0, sizeof buf);
-	memset(&(cliente.sin_zero),'\0',8);
+	while(1){
 
-	/*Creo estructura server*/
 
-	server = crear_estructura_server(portnum);
-
-	/*socket()*/
-
-	if((listener=socket(AF_INET,SOCK_STREAM,0))==-1)
-	{
-		perror("socket");
-		exit(1);
 	}
-
-	/*bind() y listen()*/
-
-	poner_a_escuchar(listener, (struct sockaddr *) &server, 1);
-
-	/*accept()*/
-
-	newfd = aceptar_conexion(listener, (struct sockaddr*) &cliente);
-
-	/*Handshake*/
-
-	bytes = recv(newfd,buf,sizeof buf,0);
-	if(bytes > 0){
-		printf("%s\n",buf);
-		}else{
-			if(bytes == -1){
-				perror("recieve");
-				exit(3);
-				}
-			if(bytes == 0){
-				printf("Se desconecto el socket: %d\n", newfd);
-				close(newfd);
-				}
-		}
-	memset(buf, 0, sizeof buf);
-	sprintf(buf, "Te has conectado a FS %d", pid);
-	send(newfd, buf, sizeof buf, 0);
-
-
-	/*recv()*/
-
-	while(1)
-		{
-			memset(buf, 0, sizeof buf);
-			bytes = recv(newfd,buf,sizeof buf,0);
-			if(bytes > 0){
-						printf("%s\n",buf);
-			}else{
-				if(bytes == -1){
-					perror("recieve");
-					exit(3);
-					}
-				if(bytes == 0){
-					printf("Se desconecto el socket: %d\n", newfd);
-					close(newfd);
-				}
-			}
-		}
-
-	/*Mostramos el mensaje recibido*/
-
-	buf[bytes_leidos]='\0';
-	printf("%s",buf);
 
 	free(cfgPath);
 
---
+++ FileSystem/socketEze.h
@@ -0,0 +1,155 @@
+#include <unistd.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/wait.h>
+#include <string.h>
+#include <commons/collections/queue.h>
+
+struct sockaddr_in initAddr(in_addr_t address,uint32_t puerto){
+	struct sockaddr_in my_addr;
+	my_addr.sin_family = AF_INET;
+	// Ordenación de máquina
+	my_addr.sin_port = htons(puerto);
+	// short, Ordenación de la red
+	my_addr.sin_addr.s_addr = address;
+	if(my_addr.sin_addr.s_addr == -1){
+		perror("BroadcastError\n");
+		exit(1);
+	}
+	memset(&(my_addr.sin_zero), '\0', 8);
+	return my_addr;
+}
+
+uint32_t enviar(uint32_t socketd, void *buf,uint32_t bytestoSend){
+	uint32_t numbytes;
+	if (numbytes = send(socketd, buf, bytestoSend, 0) <= 0){
+		perror("fail to SEND");
+	}
+	return numbytes;
+}
+
+uint32_t recibir(uint32_t socketd, void *buf,uint32_t bytestoRecv){
+	uint32_t numbytes =recv(socketd, buf, bytestoRecv, 0);
+	if(numbytes <=0){
+		if ((numbytes) <0) {
+			perror("recv");	
+		}else{
+			printf("DESCONECTADO socket %d\n",socketd);
+		}
+		close(socketd);
+	}
+	return numbytes;
+}
+uint32_t initSocket(void){
+	uint32_t sockfd;
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);// ¡Comprueba que no hay errores!
+	if(sockfd ==-1 ){
+		perror("Socket:");
+		exit(1);
+	}
+	return sockfd;
+}
+void setsocket(uint32_t sockfd,uint32_t *num){
+	if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, num,sizeof(*num)) == -1) {
+		perror("setsockopt");
+		exit(1);
+	}
+}
+void bindeo(uint32_t sockfd,struct sockaddr_in *hostServer ){
+	if (bind(sockfd, (struct sockaddr *)hostServer, sizeof(*hostServer)) == -1) {
+		perror("bind");
+		exit(1);
+	}
+}
+
+void makeConnection(uint32_t sockfd,struct sockaddr_in *hostServer,uint32_t handshake){
+	if (connect(sockfd, (struct sockaddr *)hostServer,sizeof(struct sockaddr)) == -1) {
+		perror("connect");
+		exit(1);
+	}
+	uint32_t numbytes;
+	uint32_t aux = handshake;
+	if(numbytes= enviar(sockfd,&aux,sizeof(aux)) <0 ){
+		perror("connect -- 2");
+		exit(1);
+	}
+	uint32_t var;
+	printf("envio HANDSHAKE\n");
+	if(numbytes = recibir(sockfd,&var,sizeof(var)) <=0){
+		if(var!=1){
+			printf("FALLA HANDSHAKE DISCONECT AND END\n");
+			exit(1);
+		}
+	}
+}
+
+void escuchando(uint32_t sockfd,uint32_t cantConnections)
+{
+	if (listen(sockfd, cantConnections) == -1) {
+		perror("listen");
+		exit(1);
+	}
+}
+
+void selecteando(uint32_t fdmax, fd_set *read_fds){
+
+	if (select(fdmax+1, read_fds, NULL, NULL, NULL) == -1) {
+		perror("select");
+		exit(1);
+	}
+}
+
+uint32_t server(uint32_t puerto,uint32_t cantidadConexiones){
+	uint32_t socketServer = initSocket();
+	uint32_t yes=1;
+	setsocket(socketServer,&yes);
+	struct sockaddr_in direccion;
+	direccion = initAddr(INADDR_ANY,puerto);
+	bindeo(socketServer,&direccion);
+	escuchando(socketServer,cantidadConexiones);
+	return socketServer;
+}
+
+uint32_t cliente(char *ip,uint32_t puerto,uint32_t handshake){
+	uint32_t sockCliente = initSocket();
+	struct sockaddr_in direccion = initAddr(inet_addr(ip),puerto);
+	makeConnection(sockCliente,&direccion,handshake);
+}
+
+uint32_t verificarPaquete(uint32_t sockCliente,uint32_t handshake){
+	uint32_t recibido;
+	uint32_t bytes = recibir(sockCliente,&recibido,sizeof(recibido));
+	if( bytes >0 && recibido==handshake){
+		return 0;
+	}else{
+		perror("FALLA -- NO SE ASOCIA -- ");
+		return -1;
+	}
+}
+uint32_t aceptarCliente(uint32_t sockfd, uint32_t handshake){
+	struct sockaddr_in remoteaddr;
+	uint32_t newfd;
+	uint32_t addrlen = sizeof(remoteaddr);
+	if ((newfd = accept(sockfd, (struct sockaddr*)&remoteaddr,&addrlen)) == -1)
+	{
+		perror("accept");
+	} else {
+		printf("aceptando A.. %s\n",inet_ntoa(remoteaddr.sin_addr));
+		if(!verificarPaquete(newfd,handshake)){
+			printf("selectserver: new connection from %s on socket %d\n", inet_ntoa(remoteaddr.sin_addr),newfd);
+			uint32_t var = 1;
+			enviar(newfd,&var,sizeof(var));
+			return newfd;
+		}
+		else{
+			perror("HandShake FAIL");
+			close(newfd);
+		}
+		return -1;
+	}
+	
+}
+
