8bb26fbfcb472ea97437552fc9533d366526e49d
Ezequiel Iriso <ezequieliriso@gmail.com>
2017-07-14 19:29:42 -0300

    con compile andando

---
+++ compile.sh
@@ -0,0 +1,11 @@
+cd Kernel
+gcc Kernel.c -o Debug/Kernel -lcommons -lpthread -lparser-ansisop 
+cd ../Memoria 
+gcc Memoria.c -o Debug/Memoria -lcommons -lpthread -lparser-ansisop 
+cd ../CPU
+gcc CPU.c -o Debug/CPU -lcommons -lpthread -lparser-ansisop 
+cd ../Consola
+gcc Consola.c -o Debug/Consola -lcommons -lpthread -lparser-ansisop 
+cd ../FileSystem
+gcc FileSystem.c -o FileSystem -lcommons -lpthread -lparser-ansisop 
+cd ..
